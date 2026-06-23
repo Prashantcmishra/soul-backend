eccc0305856c012efc2a0c45f93de3d9b87e2b40
Prashant Mishra <Prashantcmishra@gmail.com>
2026-06-23 13:34:40 +0530

    feat : done for done

--- src/models/Message.js
+++ src/models/Message.js
@@ -6,7 +6,7 @@ const messageSchema = new mongoose.Schema({
   text:      { type: String, required: true },
   delivered: { type: Boolean, default: false },
   seen:      { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now  , expires: 600}
+  createdAt: { type: Date, default: Date.now  , expires: 3600}
 });
 
 module.exports = mongoose.model('Message', messageSchema);
